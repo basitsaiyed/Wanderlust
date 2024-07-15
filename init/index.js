57fd123d6ad2089da50654e738e41a41652477ef
Basit <basitsaiyed19123@gmail.com>
2024-07-15 20:34:31 +0530

    First commit

---
+++ init/index.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Listing = require('../models/Listing');
+const initData = require("./data");
+
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main().then(() => console.log("connected to DB")).catch(err => { console.log(err) });
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log("data Intialized");
+}
+
+initDB();
