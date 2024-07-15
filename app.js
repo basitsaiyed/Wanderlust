3c8802d38eda805c8d2d78f9241dde09c4051877
Basit <basitsaiyed19123@gmail.com>
2024-07-15 22:16:20 +0530

    Created Navbar using ejs-mate

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const Listing = require("./models/Listing");
-const methodOverride = require("method-override")
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname,"/public")))
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
